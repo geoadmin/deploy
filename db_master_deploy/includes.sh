a6db823a70a91164f28d7099cc463e92c41a0378
Marcel Clausen <marcel.clausen@swisstopo.ch>
2015-05-06 09:35:11 +0200

    remove geodata user restriction anyone can use the script with his own user

--- db_master_deploy/includes.sh
+++ db_master_deploy/includes.sh
@@ -142,10 +142,3 @@ check_env() {
 
 # check for env variables
 check_env
-
-# force geodata
-if [[ $(whoami) != "geodata" ]]; 
-then 
-    echo "This script must be run as geodata!" >&2
-    exit 1
-fi
