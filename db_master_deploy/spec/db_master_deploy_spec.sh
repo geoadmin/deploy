86f870ef655a75a24cc3535e455d44020304d953
Marcel Clausen <marcel.clausen@swisstopo.ch>
2020-04-17 16:49:46 +0200

    test basic functions from deploy.sh

--- db_master_deploy/spec/db_master_deploy_spec.sh
+++ db_master_deploy/spec/db_master_deploy_spec.sh
@@ -2,10 +2,66 @@
 # shell output is english
 export LC_ALL=C
 
+# global functions and variables
+deploy_config="$(pwd)/deploy.cfg"
+deploy_config_backup="$(pwd)/deploy.cfg.orig"
+
+mock_set_up() {
+  mock_tear_down || :
+  if [ -f "${deploy_config}" ]; then
+    mv -f "${deploy_config}" "${deploy_config_backup}"
+  fi
+  touch "${deploy_config}"
+}
+
+mock_tear_down() {
+  reset_env
+  if [ -f "${deploy_config_backup}" ]; then
+    mv -f "${deploy_config_backup}" "${deploy_config}"
+  fi
+}
+
+# overwrite whoami
+whoami() {
+  echo -n "geodata"
+}
+
+default_env() {
+  PGUSER="www-data"
+  SPHINX_DEV="ip-dev"
+  SPHINX_INT="ip-int"
+  SPHINX_PROD="ip-prod-1 ip-prod-2"
+  SPHINX_DEMO="ip-demo"
+  PUBLISHED_SLAVES="ip-1|ip-2|ip-3|ip-4"
+}
+
+add_deploy_config() {
+  cat << EOF > ${deploy_config}
+export SPHINX_DEV="10.220.4.141"
+export SPHINX_DEMO="10.220.4.145" #DEMO == DEV since at the moment not demo instance is active
+export SPHINX_INT="10.220.5.245"
+export SPHINX_PROD="10.220.5.253 10.220.6.26"
+export PGUSER=pgkogis
+#                        <-------pg.bgdi.ch------> <--pg-sandbox.bgdi.ch-->
+export PUBLISHED_SLAVES="10.220.5.122|10.220.6.137|10.220.5.87|10.220.6.129"
+EOF
+}
+
+reset_env() {
+  unset PGUSER
+  unset SPHINX_DEV
+  unset SPHINX_INT
+  unset SPHINX_PROD
+  unset SPHINX_DEMO
+  unset PUBLISHED_SLAVES
+}
+
+source_code() {
+  source ./includes.sh
+}
+
+# includes.sh unit tests
 Describe 'includes.sh'
-  source_code() {
-    source ./includes.sh
-  }
   Describe 'variables'
     Example 'default values'
       # each Example block will be run in a subshell
@@ -32,57 +88,6 @@ Describe 'includes.sh'
     End
   End
   Describe 'check_env'
-    mock_set_up() {
-      mock_dir="mock_data"
-      MY_DIR="$(pwd)/${mock_dir}"
-      mock_tear_down || :
-      mkdir -p ${MY_DIR}
-    }
-
-    mock_tear_down() {
-      reset_env
-      rm -rf ${MY_DIR} || :
-    }
-
-    # overwrite readlink function
-    readlink() {
-      echo -n "${MY_DIR}/dummy.sh"
-    }
-
-    # overwrite whoami
-    whoami() {
-      echo -n "geodata"
-    }
-
-    default_env() {
-      PGUSER="www-data"
-      SPHINX_DEV="ip-dev"
-      SPHINX_INT="ip-int"
-      SPHINX_PROD="ip-prod-1 ip-prod-2"
-      SPHINX_DEMO="ip-demo"
-      PUBLISHED_SLAVES="ip-1|ip-2|ip-3|ip-4"
-    }
-
-    add_deploy_config() {
-      cat << EOF > "${MY_DIR}/deploy.cfg"
-export SPHINX_DEV="10.220.4.141"
-export SPHINX_DEMO="10.220.4.145" #DEMO == DEV since at the moment not demo instance is active
-export SPHINX_INT="10.220.5.245"
-export SPHINX_PROD="10.220.5.253 10.220.6.26"
-export PGUSER=pgkogis
-#                        <-------pg.bgdi.ch------> <--pg-sandbox.bgdi.ch-->
-export PUBLISHED_SLAVES="10.220.5.122|10.220.6.137|10.220.5.87|10.220.6.129"
-EOF
-    }
-
-    reset_env() {
-      unset PGUSER
-      unset SPHINX_DEV
-      unset SPHINX_INT
-      unset SPHINX_PROD
-      unset SPHINX_DEMO
-      unset PUBLISHED_SLAVES
-    }
     source_code
     mock_set_up
     Example 'no deploy.cfg and no env'
@@ -175,4 +180,41 @@ EOF
     # remove mock folders and env
     mock_tear_down
   End
+End
+
+
+# deploy.sh unit tests
+Describe 'deploy.sh'
+  source_code() {
+    source ./deploy.sh
+  }
+  mock_set_up
+  add_deploy_config
+  Describe 'functions'
+    source_code
+    Describe 'check_source'
+      target_db="testdb_dev"
+      source_db="testdb_dev"
+      Example 'same source and target'
+        When run check_source
+        The stderr should start with 'You may not copy a db or table over itself'
+        The status should be failure
+      End
+      Example 'source is not _master - n'
+        target_db="testdb_prod"
+        answer="n"
+        When run check_source
+        The stdout should start with 'Master is not the selected source'
+        The status should be failure
+      End
+      Example 'source is not _master - y'
+        target_db="testdb_prod"
+        answer="y"
+        When run check_source
+        The stdout should start with 'Master is not the selected source'
+        The status should be success
+      End
+    End
+  End
+  mock_tear_down
 End
